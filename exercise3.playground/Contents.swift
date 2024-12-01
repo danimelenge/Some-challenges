803a87297032e5bef207bddab134fd42804ba7a8
danimelenge <danielmelenge@gmail.com>
2024-12-01 10:41:06 -0500

    💍

--- exercise3.playground/Contents.swift
+++ exercise3.playground/Contents.swift
@@ -1,5 +1,22 @@
 import Foundation
 
+/*
+ * EJERCICIO:
+ * ¡La temporada 2 de "Los Anillos de Poder" está a punto de estrenarse!
+ * ¿Qué pasaría si tuvieras que encargarte de repartir los anillos
+ * entre las razas de la Tierra Media?
+ * Desarrolla un programa que se encargue de distribuirlos.
+ * Requisitos:
+ * 1. Los Elfos recibirán un número impar.
+ * 2. Los Enanos un número primo.
+ * 3. Los Hombres un número par.
+ * 4. Sauron siempre uno.
+ * Acciones:
+ * 1. Crea un programa que reciba el número total de anillos
+ *    y busque una posible combinación para repartirlos.
+ * 2. Muestra el reparto final o el error al realizarlo.
+ */
+
 func esPrimo(_ numero: Int) -> Bool {
     if numero < 2 { return false }
     for i in 2..<Int(sqrt(Double(numero))) + 1 {
