e9b6d165f700e971ece9aee07138337f082714c8
danimelenge <danielmelenge@gmail.com>
2024-12-13 10:46:43 -0500

    Morse 🐍

--- exercise7.py
+++ exercise7.py
@@ -1,3 +1,8 @@
+# Enunciado: Crea un programa que sea capaz de transformar texto natural a código morse y viceversa.
+# - Debe detectar automáticamente de qué tipo se trata y realizar la conversión.
+# - En morse se soporta raya "—", punto ".", un espacio " " entre letras o símbolos y dos espacios entre palabras "  ".
+# - El alfabeto morse soportado será el mostrado en https://es.wikipedia.org/wiki/Código_morse.
+
 # Diccionario de texto a código Morse
 morse_alphabet = {
     "A": ".-", "B": "-...", "C": "-.-.", "D": "-..", "E": ".",
