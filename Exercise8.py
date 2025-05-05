6641180d48418fcb60998c4e62e08bfa392b01c5
danimelenge <112286994+danimelenge@users.noreply.github.com>
2025-05-05 06:28:56 -0500

    Update Exercise8.py

--- Exercise8.py
+++ Exercise8.py
@@ -3,7 +3,7 @@
 # * - Recibirá por parámetros los 2 números a sumar y los segundos que
 # *   debe tardar en finalizar su ejecución.
 # * - Si el lenguaje lo soporta, deberá retornar el resultado de forma
-# *   asíncrona, es decir, sin detener la ejecución del programa principal.
+# *   asíncrona, es decir, sin detener la ejecución del programa principal.   
 # *   Se podría ejecutar varias veces al mismo tiempo.
   
 import asyncio
