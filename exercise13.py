3b8443ee50426adec7cf009b875cfbeb4251f77b
danimelenge <112286994+danimelenge@users.noreply.github.com>
2025-06-04 00:30:50 -0500

    Update exercise13.py

--- exercise13.py
+++ exercise13.py
@@ -2,7 +2,7 @@
 Procesa pagos por internet con tarjeta de crédito en más de 135 divisas.
 https://stripe.com/es
      
-- Cómo recoger información del usuario.  
+- Cómo recoger información del usuario.   
 - Cómo realizar un cargo asociado a un importe. 
 - Gestión de productos y precios. 
 - Manejo de errores.  
