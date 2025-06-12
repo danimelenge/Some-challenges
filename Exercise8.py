503b7977271ee9371b1eb8e6d45863aa555820d6
danimelenge <112286994+danimelenge@users.noreply.github.com>
2025-06-12 15:49:16 -0500

    Update Exercise8.py

--- Exercise8.py
+++ Exercise8.py
@@ -6,7 +6,7 @@
 # *   asíncrona, es decir, sin detener la ejecución del programa principal.   
 # *   Se podría ejecutar varias veces al mismo tiempo.
   
-import asyncio
+import asyncio 
 
 # Función asíncrona para sumar dos números después de un retraso
 async def async_sum(a, b, delay_seconds):
